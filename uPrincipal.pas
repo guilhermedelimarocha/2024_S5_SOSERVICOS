b4ac802ba3788f7c085c56f95e4d9fdb3fa7ad0c
nicolasbertc <nicolasbertc@gmail.com>
2024-06-09 15:45:19 -0300

    .

--- uPrincipal.pas
+++ uPrincipal.pas
@@ -4,10 +4,15 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
+  FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   Tfrm_Login = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Edit1: TEdit;
+    Edit2: TEdit;
   private
     { Private declarations }
   public
