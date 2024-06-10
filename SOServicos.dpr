d1e570287ba649ce6251152947a5d12f3bbda35d
guilhermedelimarocha <contato.guilhermedelima@gmail.com>
2024-06-10 19:08:40 -0300

    Tela de Pedir Socorro

--- SOServicos.dpr
+++ SOServicos.dpr
@@ -3,12 +3,14 @@ program SOServicos;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  uPrincipal in 'uPrincipal.pas' {frm_Login};
+  uPrincipal in 'uPrincipal.pas' {frm_PedirSocorro},
+  uAceitarSocorro in 'uAceitarSocorro.pas' {frm_AceitarSocorro};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(Tfrm_Login, frm_Login);
+  Application.CreateForm(Tfrm_PedirSocorro, frm_PedirSocorro);
+  Application.CreateForm(Tfrm_AceitarSocorro, frm_AceitarSocorro);
   Application.Run;
 end.
---
+++ uAceitarSocorro.pas
@@ -0,0 +1,25 @@
+unit uAceitarSocorro;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  Tfrm_AceitarSocorro = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frm_AceitarSocorro: Tfrm_AceitarSocorro;
+
+implementation
+
+{$R *.fmx}
+{$R *.iPhone55in.fmx IOS}
+
+end.
